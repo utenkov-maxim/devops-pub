ce791606dc4bd6b7bed177212e979896c239c48e
utenkov <utenkov@list.ru>
2022-04-07 17:19:53 +0300

    new version

--- homework7.sh
+++ homework7.sh
@@ -11,16 +11,19 @@ curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compo
 chmod +x /usr/local/bin/docker-compose
 
 # Prepare work dirs and downloan java sources from github
-mkdir /tmp/repos
-mkdir /tmp/webapps
-cd /tmp/repos
-git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
+#mkdir /tmp/repos
+#mkdir /tmp/webapps
+#cd /tmp/repos
+#git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
 
 # Prepare docker configuration
-mkdir /tmp/work
-cd /tmp/work
+#mkdir /tmp/work
+#cd /tmp/work
 git clone https://github.com/utenkov-maxim/devops-pub.git
 cd devops-pub/homework7/
+#cd devops-pub/homework7_v2/
 
 # RUN docker-compose
 docker-compose up -d
+
+#http://51.250.96.235:8080/hello-1.0/
